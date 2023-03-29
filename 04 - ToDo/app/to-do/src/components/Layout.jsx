a806b23a15e8e6afb0287847f235aa8709f72c51
Pavel <paveloland@hotmail.com>
2023-03-29 08:34:47 -0600

    react.js developing ToDo app
    React.js study session and developing ToDo app.

--- 04 - ToDo/app/to-do/src/components/Layout.jsx
+++ 04 - ToDo/app/to-do/src/components/Layout.jsx
@@ -1,14 +1,28 @@
+import { useState } from 'react';
 import { Header, Input, ListItem, ListCheck, ListControls } from './';
 
 export default function Layout() {
     
+    const [todos, setTodos] = useState([
+        {
+            id: 1,
+            title: 'Fist To do Item',
+            isCompleted: false
+        },
+        {
+            id: 2,
+            title: 'Second To do Item',
+            isCompleted: false
+        },
+    ]);
+    
     return (
         <div className='relative flex flex-col justify-center items-center h-screen'>
-            <div className='relative w-1/2 flex-grow-0 flex-shrink basis-auto p-8 bg-white rounded-lg shadow-md'>
+            <div className='relative w-2/5 flex-grow-0 flex-shrink basis-auto p-8 bg-white rounded-lg shadow-md'>
 
                 <Header label='Todo App' />
-                <Input />
-                <ListItem />
+                <Input items={todos} setItems={setTodos} />
+                <ListItem items={todos} setItems={setTodos} />
                 <ListCheck />
                 <ListControls />
 
--- 04 - ToDo/app/to-do/src/components/ListItem.jsx
+++ 04 - ToDo/app/to-do/src/components/ListItem.jsx
@@ -1,20 +1,26 @@
 import { Checkbox } from '@mui/material';
 import ClearOutlinedIcon from '@mui/icons-material/ClearOutlined';
 
-export default function ListItem() {
+export default function ListItem(props) {
+
+    function deleteItem(id) {
+        props.setItems([...props.items].filter(item => item.id !== id));
+    }
 
     return (
         <div className='relative my-4'>
 
-            <div className='relative flex justify-between items-center my-2'>
+            {props.items.map(item => (
+            <div className='relative flex justify-between items-center my-2' key={item.id}>
                 <div className='relative flex-grow-0 flex-shrink basis-11 text-cente'>
                     <Checkbox />
                 </div>
                 <div className='relative flex-1 px-1 text-left'>
-                    <p>Item</p>
+                    <p className={item.isCompleted ? 'line-through' : null}>{item.title}</p>
                 </div>
                 <div className='relative flex-grow-0 flex-shrink basis-8 text-cente'>
                     <ClearOutlinedIcon
+                        onClick={() => deleteItem(item.id)}
                         sx={{
                             padding: '5px',
                             fontSize: '30px',
@@ -29,6 +35,7 @@ export default function ListItem() {
                     />
                 </div>
             </div>
+            ))}
 
         </div>
     )
